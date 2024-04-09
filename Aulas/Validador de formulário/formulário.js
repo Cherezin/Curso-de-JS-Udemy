cae94f0abfcb9af720756a5161a6770c174948de
Chere <alexandre2405@gmail.com>
2024-04-09 19:12:39 -0300

    Começando exerxício de formulário

---
+++ Aulas/Validador de formulário/formulário.js
@@ -0,0 +1,14 @@
+class ValidaFormulário{
+    constructor(){
+        this.form = document.querySelector('form')
+
+
+        this.eventos();
+    }
+
+    eventos(){
+
+    }
+}
+
+const valida = new ValidaFormulário()
