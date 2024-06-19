f4b508bbbcdd0f59f5a9843411f419bbe046eec6
Chere <alexandre2405@gmail.com>
2024-06-19 19:44:55 -0300

    Aula de ajax

--- Aulas/ajax/script.js
+++ Aulas/ajax/script.js
@@ -1,9 +1,29 @@
-const ajax = obj => {
-    const xhr = new XMLHttpRequest()
-    xhr.open(obj.method, obj.url, true)
-    xhr.send(null)
+document.addEventListener('click', e => {
+    const el = e.target
+    const tag = el.tagName.toLowerCase()
 
-    xhr.addEventListener('load', () => {
-        
-    })
+    if(tag === 'a'){
+        e.preventDefault()
+        carregaPagina(el)
+    }
+})
+
+async function carregaPagina(el){
+    try{
+        const href = el.getAttribute('href')
+    const response = await fetch(href)
+
+    if(response.status !== 200) throw new Error('ERROR 404.')
+    
+    const html = await response.text()
+    
+    carregaResultado(html)
+    } catch(e){
+        console.log(e)
+    }
+}
+
+function carregaResultado(response){
+    const resultado = document.querySelector('.resultado')
+    resultado.innerHTML = response
 }
