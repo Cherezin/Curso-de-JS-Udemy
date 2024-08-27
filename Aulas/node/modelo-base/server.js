732e8d1a0ac663386d75e8a245b49d1c89a76293
Chere <alexandre2405@gmail.com>
2024-08-27 19:06:51 -0300

    adicionando uso do json com express

--- Aulas/node/modelo-base/server.js
+++ Aulas/node/modelo-base/server.js
@@ -22,6 +22,7 @@ const csrf = require('csurf')
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware')
 
 app.use(helmet())
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'public')))
 
